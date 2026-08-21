ae5a02fb889a54e48cb056201c6f51d335bd620e
funghwang99 <funghwang99@gmail.com>
2026-08-22 01:46:14 +0700

    Point Baggio archive to valid binary image

--- puskas-preview.js
+++ puskas-preview.js
@@ -25,7 +25,7 @@
     },
     baggio: {
       href: 'the-second-arrow.html',
-      src: 'https://raw.githubusercontent.com/funghwang99/notes-from-n5/main/baggio-1994-archive.jpg',
+      src: 'baggio-1994-archive.jpg?v=20260822-baggio-binary-1',
       alt: 'Roberto Baggio tại World Cup 1994.',
       position: 'center 42%',
     },
