017d3dd4a90f163a9b57040b04ddb3d1a144c415
funghwang99 <funghwang99@gmail.com>
2026-08-22 10:04:56 +0700

    Use WC94 Baggio image in archive

--- baggio-archive-fix.js
+++ baggio-archive-fix.js
@@ -1,12 +1,12 @@
 (() => {
-  const BAGGIO_ARCHIVE_IMAGE = "https://commons.wikimedia.org/wiki/Special:Redirect/file/Roberto_Baggio_-_Italia_%2790.jpg?width=1000";
+  const BAGGIO_ARCHIVE_IMAGE = "https://retrosoccerkits.com/cdn/shop/files/IMG_9105.jpg?v=1683804239&width=1946";
 
   const apply = () => {
     const image = document.querySelector('.archive#archive a[href="the-second-arrow.html"] img');
     if (!image) return;
     if (image.src !== BAGGIO_ARCHIVE_IMAGE) image.src = BAGGIO_ARCHIVE_IMAGE;
-    image.alt = 'Roberto Baggio trong màu áo Italy trước World Cup 1990.';
-    image.style.objectPosition = 'center 24%';
+    image.alt = 'Roberto Baggio trong màu áo sân nhà của Italy tại World Cup 1994.';
+    image.style.objectPosition = 'center 28%';
   };
 
   apply();
