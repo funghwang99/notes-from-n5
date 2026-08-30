8e455cfa20e64a87184292cd3cb1d3c51359feda
funghwang99 <funghwang99@gmail.com>
2026-08-30 12:25:47 +0700

    Refresh Charlton publisher loader

--- best-link-fix.js
+++ best-link-fix.js
@@ -22,7 +22,7 @@
   if (!window.__N5_CHARLTON_PUBLISH_LOADER__) {
     window.__N5_CHARLTON_PUBLISH_LOADER__ = true;
     const publish = document.createElement('script');
-    publish.src = 'charlton-publish.js?v=20260830-charlton-1';
+    publish.src = 'charlton-publish.js?v=20260830-charlton-2';
     publish.async = false;
     document.head.append(publish);
   }
