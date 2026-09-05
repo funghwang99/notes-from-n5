03ff7a407c4ec48e94d331319d8dd3dacab6ac9f
funghwang99 <funghwang99@gmail.com>
2026-09-05 12:33:04 +0700

    Bump Bruno publisher for 3-1-6 rotation build

--- baresi-publish.js
+++ baresi-publish.js
@@ -62,7 +62,7 @@
   if (!window.__N5_BRUNO_PUBLISH_LOADER__) {
     window.__N5_BRUNO_PUBLISH_LOADER__ = true;
     const publish = document.createElement('script');
-    publish.src = 'bruno-publish.js?v=20260905-bruno-2';
+    publish.src = 'bruno-publish.js?v=20260905-bruno-3';
     publish.async = false;
     document.head.append(publish);
   }
